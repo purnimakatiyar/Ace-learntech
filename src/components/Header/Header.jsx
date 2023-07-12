c1edb618fb5f2aac447114d79a0c44090ce497a4
purnimakatiyar <purnimakatiyar4@gmail.com>
2023-07-12 14:55:07 +0530

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+
+import "./header.css";
+
+
+const nav__links=[
+    {
+        path:'#home',
+        display:'Home'
+    },
+    {
+        path:'#about',
+        display: 'About'
+    },
+    {
+        path: '#services',
+        display:'Services'
+    },
+    {
+        path: '#other',
+        display: 'Other'
+    },
+    {
+        path: '#updates',
+        display: 'Updates'
+    },
+    {   path: '#faqs',
+        display: 'FAQs'
+    },
+    {   path: '#contact',
+        display: 'Contact'
+    }
+
+
+
+]
+
+
+
+const Header = () => {
+  return (
+   <header className="header">
+    <div className="container">
+        <div className="nav__wrapper">
+            <div className="logo">
+                <h2>Learntech</h2>
+              
+</div>
+
+                {/* navigation=================== */}
+<div className="navigation">
+    <ul className="menu">
+{nav__links.map((item,index)=>(
+    <li className="menu-item"><a href={item.path} className="menu__link">{item.display}</a></li>
+))}
+    </ul>
+</div>
+
+{/* light-mode================== */}
+<div className="light__mode">
+    <span><i class="ri-sun-line"></i>Light Mode</span>
+</div>
+            </div>
+        </div>
+  
+   </header>   
+  )
+}
+
+export default Header
