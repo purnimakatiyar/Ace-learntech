5b5be31fbbdc56774822676e1dfa1b390db6d207
Niharika <saininiharika123@gmail.com>
2023-07-13 16:05:58 +0530

    added file

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@
 import './App.css';
 
 import Header from "./components/Header/Header";
+import Home from './components/Home/home';
 // import Main from "./Main";
 import Main from "./components/Main"
 
 function App() {
   return (
     <>
-      <Header />,
-      <Main/>,
+      <Header />
+      <Home/>
+      <Main/>
 
     </>
   )
