a17fd2a6c2cde46ef3388cf883a52845110acd2a
purnimakatiyar <purnimakatiyar4@gmail.com>
2023-07-15 14:06:53 +0530

    new additions2

---
+++ src/components/UI/Services.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../styles/services.css';
+
+
+const Services = () => {
+  return <section id="service">
+    <div className="container">
+        <div className="services__top-content">
+            <h6 className="subtitle">Our Services</h6>
+            <h2>Make the most of your time by utilizing our website.</h2>
+        </div>
+    </div>
+  </section>
+};
+
+export default Services;
